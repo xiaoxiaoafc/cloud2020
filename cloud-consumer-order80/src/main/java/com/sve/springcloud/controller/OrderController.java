783d29ff6e82c68ed0a50dd45c958346528f0e4e
chenshuhong520 <1048369112@qq.com>
2020-05-28 23:37:31 +0800

    初次提交

--- cloud-consumer-order80/src/main/java/com/sve/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/sve/springcloud/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.sve.springcloud.controller;
 import com.sve.springcloud.entities.CommonResult;
 import com.sve.springcloud.entities.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -17,5 +19,11 @@ public class OrderController {
     @PostMapping("create")
     public CommonResult<Payment> create(Payment payment){
         return  restTemplate.postForObject(PAYMENT_URL + "/payment/create",payment,CommonResult.class);
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id)
+    {
+        return  restTemplate.getForObject(PAYMENT_URL + "/payment/get/"+ id,CommonResult.class);
     }
 }
